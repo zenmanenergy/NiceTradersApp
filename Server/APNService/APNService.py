c9afbe213a68de68846c647479ed2ac7ddb0d737
Steve Nelson <snelson@zenmanenergy.org>
2025-12-25 09:36:21 -0500

    apn

--- Server/APNService/APNService.py
+++ Server/APNService/APNService.py
@@ -32,7 +32,7 @@ class APNService:
         self.key_id = key_id
         self.team_id = team_id
         self.topic = topic or "NiceTraders.Nice-Traders"
-        self.use_sandbox = True  # Set to False for production
+        self.use_sandbox = True  # Development builds use sandbox
     
     def send_notification(self, user_id, title, body, badge=1, sound='default', 
                          session_id=None, deep_link_type=None, deep_link_id=None, device_id=None):
